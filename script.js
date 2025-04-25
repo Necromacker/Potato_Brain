0a4ed0710d3ea46c24af4c40df749477611eb711
Krishna Gorde <krishnagorde04@gmail.com>
2025-04-25 11:03:16 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -196,7 +196,7 @@ const storeScore = async () => {
 // Fetch leaderboard from backend
 const fetchLeaderboard = async () => {
     try {
-        const response = await fetch('http://localhost:3000/api/leaderboard'); // Adjust if backend URL differs
+        const response = await fetch('https://trivia-4bb4.onrender.com'); // Adjust if backend URL differs
         const leaderboardData = await response.json();
 
         leaderboardData.sort((a, b) => b.score - a.score);
